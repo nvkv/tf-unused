8231e5e4a339ec5e48ce36821f0f2bd106887b94
Semyon Novikov <novikov@fastmail.com>
2019-09-22 14:27:30 +0500

    Convert indentation to spaces

--- scripts/travis_build.sh
+++ scripts/travis_build.sh
@@ -7,51 +7,51 @@ set -eo pipefail
 OS="$1"
 
 if [ "${OS}" != "linux" ] && [ "${OS}" != "osx" ]; then
-	echo "OS is not specified of unknown ${OS}"
-	exit 1
+    echo "OS is not specified of unknown ${OS}"
+    exit 1
 fi
 
 function install_musl_target() {
-	if [ "${OS}" != "linux" ]; then
-		echo "Skipping musl target install on non-linux os"
-		return 0
-	fi
-	rustup target add x86_64-unknown-linux-musl
+    if [ "${OS}" != "linux" ]; then
+        echo "Skipping musl target install on non-linux os"
+        return 0
+    fi
+    rustup target add x86_64-unknown-linux-musl
 }
 
 function run_tests() {
-	cargo test --verbose --all
+    cargo test --verbose --all
 }
 
 function build_osx_release() {
-	cargo build --release
-	cp ./target/release/tf-unused .
+    cargo build --release
+    cp ./target/release/tf-unused .
 }
 
 function build_linux_release() {
-	RUSTFLAGS=-Clinker=musl-gcc cargo build --release --target=x86_64-unknown-linux-musl
-	cp ./target/x86_64-unknown-linux-musl/release/tf-unused .
+    RUSTFLAGS=-Clinker=musl-gcc cargo build --release --target=x86_64-unknown-linux-musl
+    cp ./target/x86_64-unknown-linux-musl/release/tf-unused .
 }
 
 function build() {
-	if [ "${OS}" == "linux" ]; then
-		build_linux_release
-	elif [ "${OS}" == "osx" ]; then
-		build_osx_release
-	else
-		echo "Unknown OS"
-		exit 1
-	fi
+    if [ "${OS}" == "linux" ]; then
+        build_linux_release
+    elif [ "${OS}" == "osx" ]; then
+        build_osx_release
+    else
+        echo "Unknown OS"
+        exit 1
+    fi
 }
 
 function pack_the_binary() {
-	if [ ! -f ./tf-unused ]; then
-		echo "ERROR: Couldn't locate compiled binary"
-		exit 1
-	fi
-	mkdir -p tf-unused-${OS}
-	mv ./tf-unused "./tf-unused-${OS}"
-	tar czf "tf-unused-${OS}.tar.gz" "./tf-unused-${OS}"
+    if [ ! -f ./tf-unused ]; then
+        echo "ERROR: Couldn't locate compiled binary"
+        exit 1
+    fi
+    mkdir -p tf-unused-${OS}
+    mv ./tf-unused "./tf-unused-${OS}"
+    tar czf "tf-unused-${OS}.tar.gz" "./tf-unused-${OS}"
 }
 
 # The Pipeline
